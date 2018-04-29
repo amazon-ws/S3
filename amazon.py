5a4bea7402f288530aaf3744bb1e3c7f33dbb93d
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2018-04-29 13:46:27 -0300

    Update amazon.py

--- amazon.py
+++ amazon.py
@@ -1,7 +1,7 @@
 import tinys3
 import time
 
-conn = tinys3.Connection('AKIAJOPLNA4GSIA2BKEA','+1gyTjTua9ym7yMPim/Bo9mNBbaRnoyo7jOfO6Sy',tls=True)
+conn = tinys3.Connection('AKIAJOPLNA4GSIA2BKEaa','+1gyTjTua9ym7yMPim/Bo9mNBbaRnoyo7jOfO6Sy',tls=True)
 f = open('/home/marlon/ionic/clodoo-ionic/python-server/server.py','rb')
 
 if conn.upload(time.strftime("%H:%M:%S") + '-server.py',f,'falconsoft-3d'):
